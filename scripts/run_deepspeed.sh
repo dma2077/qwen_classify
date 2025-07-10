63b56344c67c8110a9f17c6b5631fdd1a06118a6
madehua <madehua@kuaishou.com>
2025-07-10 22:27:42 +0800

    fix

--- scripts/run_deepspeed.sh
+++ scripts/run_deepspeed.sh
@@ -6,6 +6,9 @@ CONFIG_FILE="configs/config.yaml"
 OUTPUT_DIR="./outputs"
 NUM_GPUS=8
 
+# Add current directory to PYTHONPATH to resolve imports
+export PYTHONPATH="${PYTHONPATH}:$(pwd)"
+
 if [[ $NUM_GPUS -eq 1 ]]; then
     python training/deepspeed_trainer.py \
         --config_file $CONFIG_FILE \
