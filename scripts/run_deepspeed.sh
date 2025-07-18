98f6e5bf512201c63f152ec0e40622e37a3d0ded
madehua <madehua@kuaishou.com>
2025-07-18 15:49:28 +0800

    1

--- scripts/run_deepspeed.sh
+++
@@ -1,2 +0,0 @@
-nohup bash /llm_reco/dehua/code/qwen_classify/scripts/run_deepspeed_ls_1e_5_ls.sh > /llm_reco/dehua/code/qwen_classify/output_deepspeed_1e_5_ls.log 2>&1
-nohup bash /llm_reco/dehua/code/qwen_classify/scripts/run_deepspeed_ls_1e_5.sh > /llm_reco/dehua/code/qwen_classify/output_deepspeed_1e_5.log 2>&1
